6695d39678c621b4209c419039a1c897a685e02b
Gerhard Steenkamp <51655063+gsteenkamp89@users.noreply.github.com>
2024-09-10 15:42:31 +0200

    Update index.ts
    Co-authored-by: Dong-Ha Kim <dongha.kim210@gmail.com>

--- packages/sdk/src/constants/index.ts
+++ packages/sdk/src/constants/index.ts
@@ -1,7 +1,7 @@
 export const MAINNET_API_URL = "https://app.across.to/api";
 export const TESTNET_API_URL = "https://testnet.across.to/api";
 
-export const MAINNET_INDEXER_API = "https://api.across.to";
+export const MAINNET_INDEXER_API = "https://public.api.across.to";
 export const TESTNET_INDEXER_API = "https://dev.public.api.across.to";
 
 export const STATUS = {
