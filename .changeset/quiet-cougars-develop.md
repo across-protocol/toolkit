b18629cf33d98ec4025a544998574a26cc58ca5f
Melisa Guevara <melisaguevarav@hotmail.com>
2025-06-09 14:17:47 -0300

    improve: use Indexer instead of Scraper (#224)
    * improve: use Indexer instead of Scraper
    
    * changeset file

---
+++ .changeset/quiet-cougars-develop.md
@@ -0,0 +1,5 @@
+---
+"@across-protocol/app-sdk": patch
+---
+
+Improve: use Indexer API instead of Scraper API
--- packages/sdk/src/constants/index.ts
+++ packages/sdk/src/constants/index.ts
@@ -1,8 +1,8 @@
 export const MAINNET_API_URL = "https://app.across.to/api";
 export const TESTNET_API_URL = "https://testnet.across.to/api";
 
-export const MAINNET_INDEXER_API = "https://public.api.across.to";
-export const TESTNET_INDEXER_API = "https://dev.public.api.across.to";
+export const MAINNET_INDEXER_API = "https://indexer.api.across.to";
+export const TESTNET_INDEXER_API = "https://dev.indexer.api.across.to";
 
 export const STATUS = {
   SUCCESS: "SUCCESS",
