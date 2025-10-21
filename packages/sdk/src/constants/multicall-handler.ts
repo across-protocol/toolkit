3e3552bf63e3e12204c45ddd9257c36e4455034d
Gerhard Steenkamp <gerhard@umaproject.org>
2025-10-21 12:37:59 +0200

    add deploys, resolve with fallback
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

---
+++ packages/sdk/src/constants/multicall-handler.ts
@@ -0,0 +1,148 @@
+import { Address } from "viem";
+
+export const multicallHandlerDeployments: Record<
+  number,
+  { address: Address; blockNumber: number }
+> = {
+  1: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 22967417,
+  },
+  10: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 138622548,
+  },
+  56: {
+    address: "0xAC537C12fE8f544D712d71ED4376a502EEa944d7",
+    blockNumber: 48762440,
+  },
+  130: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 22350961,
+  },
+  232: {
+    address: "0x1Ed0D59019a52870337b51DEe8190486a8663037",
+    blockNumber: 3458452,
+  },
+  324: {
+    address: "0x68d3806E57148D6c6793C78EbDDbc272fE605dbf",
+    blockNumber: 63168917,
+  },
+  480: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 16881850,
+  },
+  690: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 20457079,
+  },
+  919: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 32616082,
+  },
+  999: {
+    address: "0x5E7840E06fAcCb6d1c3b5F5E0d1d3d07F2829bba",
+    blockNumber: 13992522,
+  },
+  1135: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 19185347,
+  },
+  1301: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 26247751,
+  },
+  1868: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 9982275,
+  },
+  4202: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 23893534,
+  },
+  8453: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 33154505,
+  },
+  9745: {
+    address: "0x5E7840E06fAcCb6d1c3b5F5E0d1d3d07F2829bba",
+    blockNumber: 1619956,
+  },
+  11155111: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 8810926,
+  },
+  11155420: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 138622548,
+  },
+  34443: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 26465760,
+  },
+  37111: {
+    address: "0x02D2B95F631E0CF6c203E77f827381B0885F7822",
+    blockNumber: 145561,
+  },
+  42161: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 360020909,
+  },
+  57073: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 19600021,
+  },
+  59144: {
+    address: "0xdF1C940487574EEfa79989a79a4936A0F979cDa2",
+    blockNumber: 21108879,
+  },
+  80002: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 24181411,
+  },
+  81457: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 22144286,
+  },
+  84532: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 28665844,
+  },
+  129399: {
+    address: "0xAC537C12fE8f544D712d71ED4376a502EEa944d7",
+    blockNumber: 3179705,
+  },
+  137: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 74229464,
+  },
+  421614: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 175845768,
+  },
+  534352: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 17441646,
+  },
+  808813: {
+    address: "0xAC537C12fE8f544D712d71ED4376a502EEa944d7",
+    blockNumber: 15392366,
+  },
+  7777777: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 33202799,
+  },
+  168587773: {
+    address: "0x0F7Ae28dE1C8532170AD4ee566B5801485c13a0E",
+    blockNumber: 24206664,
+  },
+  //   TODO: support SVM
+  //   34268394551451: {
+  //     address: "HaQe51FWtnmaEcuYEfPA7MRCXKrtqptat4oJdJ8zV5Be",
+  //     blockNumber: 349358090,
+  //   },
+  //   133268194659241: {
+  //     address: "Fk1RpqsfeWt8KnFCTW9NQVdVxYvxuqjGn6iPB9wrmM8h",
+  //     blockNumber: 356321050,
+  //   },
+};
--- packages/sdk/src/utils/multicallHandler.ts
+++ packages/sdk/src/utils/multicallHandler.ts
@@ -1,23 +1,17 @@
 import { Address, encodeAbiParameters, parseAbiParameters } from "viem";
 
 import { CrossChainAction } from "../types/index.js";
+import { multicallHandlerDeployments } from "../constants/multicall-handler.js";
 
 export type BuildMessageParams = {
   fallbackRecipient: Address;
   actions: CrossChainAction[];
 };
 
-export function getMultiCallHandlerAddress(chainId: number) {
-  // @todo: use sdk or API to source addresses?
+export function getMultiCallHandlerAddress(chainId: number): Address {
   const defaultAddress = "0x924a9f036260DdD5808007E1AA95f08eD08aA569";
-  switch (chainId) {
-    case 324:
-      return "0x863859ef502F0Ee9676626ED5B418037252eFeb2";
-    case 59144:
-      return "0x1015c58894961F4F7Dd7D68ba033e28Ed3ee1cDB";
-    default:
-      return defaultAddress;
-  }
+  const deployments = multicallHandlerDeployments;
+  return deployments?.[chainId]?.address ?? defaultAddress;
 }
 
 export function buildMulticallHandlerMessage(params: BuildMessageParams) {
